8ce9e96cbb8d4a82a9f04b7bc4b624855bd86e15
todaylg <2489437918@qq.com>
2016-08-27 13:22:35 +0800

    去掉广告

--- LG.js
+++ LG.js
@@ -2,27 +2,47 @@
  * Created by LG on 2016-08-18.
  */
  $(function () {
- var btn_wrap = $(".bgray-btn-wrap");
-    if(btn_wrap.length){
-    	//1,去掉广告
-    	var ad_node =$(".bilibili-player-promote-wrap");
-    	ad_node.remove();
-    	//2,如果选择的是Flash，提示是否选择HTML5
-    	if($(".bgray-btn-wrap").children(".active").length){
-    		if($(".bgray-btn-wrap").children(".active").text()=="Flash播放器"){
-    			if(confirm("现在的默认播放器是Flash播放器,更改为HTML5播放器吗？")){
-				    $(".bgray-btn-wrap div:first-child").click();
-    				setTimeout((function () {
-    					ad_node.remove();
-    				}), 5000)
-				}
-    		}
-    	}
-    }else{
-    	//跳转到打开HTML5开关的页面提示用户选择
-    	if(confirm("您还没有设置HTML5播放器作为默认播放器哦，前往设置吗？")){
-		   window.open("http://www.bilibili.com/html/help.html#p");
-		}
-    }
-    console.log("bilibili-launch_HTML5 is runing...");
+   var btn_wrap = $(".bgray-btn-wrap");
+
+   function Recheck () {
+    console.log('还没刷出广告，进行Recheck()')
+    var ad_node_check =$(".bilibili-player-promote-wrap");
+    setTimeout(function () {
+        if(!ad_node_check.length){
+            Recheck ();
+        }else{
+            console.log('干掉广告！！')
+            ad_node_check.remove();
+        }
+    }, 1000);
+}
+if(btn_wrap.length){
+        //1,去掉广告
+        var ad_node =$(".bilibili-player-promote-wrap");
+        if(ad_node.length){
+            console.log('广告走开！！');
+            ad_node.remove();
+        }else{
+                //选的是flash播放器也会走这
+                //如果选择的是Flash，提示是否选择HTML5
+                if($(".bgray-btn-wrap").children(".active").length){
+                    if($(".bgray-btn-wrap").children(".active").text()=="Flash播放器"){
+                        if(confirm("现在的默认播放器是Flash播放器,更改为HTML5播放器吗？")){
+                            $(".bgray-btn-wrap div:first-child").click();
+                            Recheck();
+                        }
+                    }
+                }
+                //网速不好，还没有刷出来
+                Recheck ();
+            }
+        }else{
+        //跳转到打开HTML5开关的页面提示用户选择
+        if(confirm("您还没有设置HTML5播放器作为默认播放器哦，前往设置吗？")){
+         window.open("http://www.bilibili.com/html/help.html#p");
+     }
+ }
+ //TODO
+ //切换到Flash播放器再切回HTML5播放器还是有广告
+ console.log("bilibili-launch_HTML5 is runing...");
 });
